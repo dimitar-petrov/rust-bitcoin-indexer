22c811f18cce1cf231ee3566e7a4637d6e529791
Dimitar Petrov <petrov.dimp@gmail.com>
2021-11-16 15:12:36 +0200

    wip: add docker provisioning
    - updates to env and Dockerfile
    
    Change-Id: Ib4dec050eeb02dc5f6c57e89b1a32d53661dfb05

---
+++ docker/init-create-dbs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+for DBNAME in $PS_DATABASES; do
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE $DBNAME;
+    GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO "$POSTGRES_USER";
+EOSQL
+    done
